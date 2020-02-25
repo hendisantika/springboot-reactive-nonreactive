6a547d646bfa680917c1e062f0fe558169d8d29b
Hendi Santika <hendisantika@gmail.com>
2020-02-25 17:56:36 +0700

    #14 - Add post in MVCAsyncController

--- src/main/java/com/hendisantika/springbootreactivenonreactive/controller/MVCAsyncController.java
+++ src/main/java/com/hendisantika/springbootreactivenonreactive/controller/MVCAsyncController.java
@@ -5,8 +5,11 @@ import com.hendisantika.springbootreactivenonreactive.repository.NonReactiveRepo
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
 import java.util.concurrent.CompletableFuture;
 
 /**
@@ -27,5 +30,10 @@ public class MVCAsyncController {
     @GetMapping("/mvcasync/{id}")
     public CompletableFuture<Message> findById(@PathVariable(value = "id") String id) {
         return CompletableFuture.supplyAsync(() -> nonReactiveRepository.findById(id).orElse(null));
+    }
+
+    @PostMapping("/mvcasync")
+    public CompletableFuture<Message> post(@Valid @RequestBody Message message) {
+        return CompletableFuture.supplyAsync(() -> nonReactiveRepository.save(message));
     }
 }
