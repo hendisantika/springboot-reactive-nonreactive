34183049fbbdefac677ee438575f0381cbeb166b
Hendi Santika <hendisantika@gmail.com>
2020-02-25 17:55:53 +0700

    #12 - Add MVCAsyncController

---
+++ src/main/java/com/hendisantika/springbootreactivenonreactive/controller/MVCAsyncController.java
@@ -0,0 +1,21 @@
+package com.hendisantika.springbootreactivenonreactive.controller;
+
+import com.hendisantika.springbootreactivenonreactive.repository.NonReactiveRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-reactive-nonreactive
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 25/02/20
+ * Time: 17.55
+ */
+@RestController
+public class MVCAsyncController {
+
+    @Autowired
+    private NonReactiveRepository nonReactiveRepository;
+}
