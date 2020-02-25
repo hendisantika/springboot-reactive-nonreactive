7d6b72d06af8d39a16a9f710dda3af1065d49e34
Hendi Santika <hendisantika@gmail.com>
2020-02-25 17:57:40 +0700

    #16 - Add findByIdReactive in WebFluxController

--- src/main/java/com/hendisantika/springbootreactivenonreactive/controller/WebFluxController.java
+++ src/main/java/com/hendisantika/springbootreactivenonreactive/controller/WebFluxController.java
@@ -1,8 +1,12 @@
 package com.hendisantika.springbootreactivenonreactive.controller;
 
+import com.hendisantika.springbootreactivenonreactive.model.Message;
 import com.hendisantika.springbootreactivenonreactive.repository.ReactiveRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
 /**
  * Created by IntelliJ IDEA.
@@ -18,5 +22,10 @@ public class WebFluxController {
 
     @Autowired
     private ReactiveRepository reactiveRepository;
+
+    @GetMapping("/webflux/{id}")
+    public Mono<Message> findByIdReactive(@PathVariable(value = "id") String id) {
+        return reactiveRepository.findById(id);
+    }
 
 }
