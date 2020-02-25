a367875e4dc30e17d67de39f06da7455adf3fad1
Hendi Santika <hendisantika@gmail.com>
2020-02-25 17:48:25 +0700

    #1 - Initial Commit

---
+++ src/test/java/com/hendisantika/springbootreactivenonreactive/SpringbootReactiveNonreactiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootreactivenonreactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootReactiveNonreactiveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
